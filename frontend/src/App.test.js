ddc5fe4ae11f2e24c7dd99eae81ec2d76736b2b2
Salim <salim.hassan.welleat@gmail.com>
2023-01-27 11:36:16 +0100

    Correct test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(
+    /Mise en place d'une CI-CD pour son projet Fil Rouge/i
+  );
   expect(linkElement).toBeInTheDocument();
 });
