b9b2b8c09ce8123efd4700819eb8049e8d20b5ca
Salim <salim.hassan.welleat@gmail.com>
2023-01-27 11:41:15 +0100

    Correction2

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -4,7 +4,7 @@ import App from "./App";
 test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(
-    /Mise en place d'une CI-CD pour son projet Fil Rouge/i
+    /Mise en place d'une CI-CD pour son projet Fil Rouge/
   );
   expect(linkElement).toBeInTheDocument();
 });
